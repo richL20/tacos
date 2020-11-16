366d977c41f770dccc03ea0d431af80fbb6807b0
richL20 <72081278+richL20@users.noreply.github.com>
2020-11-16 08:04:59 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('.hamburger-menu').on('click', function () {
+    $('.toggle').toggleClass('open');
+    $('.nav-list').toggleClass('open');
+
+  });
+
+  AOS.init({
+    easing:'ease',
+    duration:1000,
+
+  });
+
+
+});
+
